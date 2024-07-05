cb1023131f0abc7008c1ce43f6d1adb5074a9ea0
Cristian Gomez Garcia <cristian.garcia@neoris.com>
2024-07-04 18:00:10 -0600

    Inicializacion de inserts de los datos

---
+++ src/main/resources/data-mysqldocker.sql
@@ -0,0 +1,11 @@
+INSERT INTO categories (name, description) VALUES 
+('Electrónica', 'Productos electrónicos como smartphones, laptops, etc.'),
+('Ropa', 'Prendas de vestir para hombres, mujeres y niños.'),
+('Libros', 'Libros de diversos géneros y autores.'),
+('Hogar', 'Artículos para el hogar, decoración y muebles.'),
+('Juguetes', 'Juguetes y juegos para niños de todas las edades.'),
+('Deportes', 'Equipamiento y ropa deportiva.'),
+('Belleza', 'Productos de belleza y cuidado personal.'),
+('Alimentos', 'Productos alimenticios, snacks y bebidas.'),
+('Mascotas', 'Artículos y alimentos para mascotas.'),
+('Tecnología', 'Últimas novedades en tecnología y gadgets.');
